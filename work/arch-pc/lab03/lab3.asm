d1775f90a7bcfda712553bd2cf6c71d4a299e44d
zizicur <159134222+zizicur@users.noreply.github.com>
2024-02-20 14:52:05 +0300

    лаба 3

---
+++ work/arch-pc/lab03/lab3.asm
@@ -0,0 +1,16 @@
+SECTION .data
+hello: DB 'Bazarbaev Aziz' ,10
+hellolen: EQU $-hello
+
+SECTION .text
+GLOBAL _start
+_start:
+mov eax,4
+mov ebx,1
+mov ecx,hello
+mov edx,hellolen
+int 80h
+
+mov eax,1
+mov ebx,0
+int 80h
